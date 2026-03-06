011df7a589b3ead805259d04919eb9e94e3c20d0
Pointyrook <erik.s.petrovich@gmail.com>
2026-03-06 15:34:32 -0800

    IP: working out color scheme

--- FinalProject-HabitApp/app/src/main/java/dev/csse/ceha/mobileapp/ui/theme/Type.kt
+++ FinalProject-HabitApp/app/src/main/java/dev/csse/ceha/mobileapp/ui/theme/Type.kt
@@ -14,7 +14,13 @@ val Typography = Typography(
     fontSize = 16.sp,
     lineHeight = 24.sp,
     letterSpacing = 0.5.sp
-  )
+  ),
+	titleLarge = TextStyle(
+			fontFamily = FontFamily.Default,
+			fontWeight = FontWeight.SemiBold,
+			fontSize = 30.sp,
+			letterSpacing = 1.sp
+	)
   /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
